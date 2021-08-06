5f0474fcbc7260e3b70045c1aec58a76c85bedc7
santiago0411 <santicarnuccio@gmail.com>
2021-08-06 01:56:56 -0300

    First commit

---
+++ OpenGL/Application.cpp
@@ -0,0 +1,212 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <sstream>
+
+#define ASSERT(x) if(!(x)) __debugbreak()
+#define GLCall(x) GLClearError();\
+	x;\
+	ASSERT(GLLogCall(#x, __FILE__, __LINE__))
+
+static void GLClearError()
+{
+    while (glGetError() != GL_NO_ERROR);
+}
+
+static bool GLLogCall(const char* function, const char* file, int line)
+{
+	while (GLenum error = glGetError())
+	{
+        std::cout << "[OpenGL Error] (" << error << "): " << function << " " << file << ":" << line << '\n';
+        return false;
+	}
+    return true;
+}
+
+struct ShaderProgramSource
+{
+    std::string VertexSource;
+    std::string FragmentSource;
+};
+
+static ShaderProgramSource ParseShader(const std::string& filepath)
+{
+    std::ifstream stream(filepath);
+
+    enum class ShaderType
+    {
+	    None = -1, Vertex = 0, Fragment = 1
+    };
+	
+    std::string line;
+    std::stringstream ss[2];
+    auto type = ShaderType::None;
+	
+	while(std::getline(stream, line))
+	{
+        if (line.find("#shader") != std::string::npos)
+        {
+            if (line.find("vertex") != std::string::npos)
+                type = ShaderType::Vertex;
+            else if (line.find("fragment") != std::string::npos)
+                type = ShaderType::Fragment;
+        }
+        else
+        {
+            ss[(int)type] << line << '\n';
+        }
+	}
+
+    return { ss[0].str(), ss[1].str() };
+}
+
+static unsigned int CompileShader(const unsigned int type, const std::string& source)
+{
+    GLCall(const unsigned int id = glCreateShader(type));
+    const char* src = source.c_str();
+    GLCall(glShaderSource(id, 1, &src, nullptr));
+    GLCall(glCompileShader(id));
+
+    int result;
+    GLCall(glGetShaderiv(id, GL_COMPILE_STATUS, &result));
+	
+	if (result == GL_FALSE)
+	{
+        int length;
+        GLCall(glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length));
+        const auto message = (char*)alloca(length * sizeof(char));
+        GLCall(glGetShaderInfoLog(id, length, &length, message));
+        std::cout << "Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << " shader!\n";
+        std::cout << message << "\n";
+        GLCall(glDeleteShader(id));
+        return 0;
+	}
+	
+    return id;
+}
+
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
+{
+    GLCall(const unsigned int program = glCreateProgram());
+    GLCall(const unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader));
+    GLCall(const unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader));
+
+    GLCall(glAttachShader(program, vs));
+    GLCall(glAttachShader(program, fs));
+    GLCall(glLinkProgram(program));
+    GLCall(glValidateProgram(program));
+
+    GLCall(glDeleteShader(vs));
+    GLCall(glDeleteShader(fs));
+
+    return program;
+}
+
+int main()
+{
+	/* Initialize the library */
+    if (!glfwInit())
+        return -1;    
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	
+    /* Create a windowed mode window and its OpenGL context */
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    glfwSwapInterval(1);
+
+    if (glewInit() != GLEW_OK)
+        std::cout << "Error!\n";
+
+    std::cout << glGetString(GL_VERSION) << "\n";
+
+    float positions[] = {
+		-0.5f,  -0.5f,
+		 0.5f,  -0.5f,
+         0.5f,   0.5f,
+        -0.5f,   0.5f
+    };
+
+    unsigned int indices[] = {
+        0, 1, 2,
+    	2, 3, 0
+    };
+
+    unsigned int vao;
+    GLCall(glGenVertexArrays(1, &vao));
+    GLCall(glBindVertexArray(vao));
+	
+    unsigned int buffer;
+    GLCall(glGenBuffers(1, &buffer));
+    GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));
+    GLCall(glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float), positions, GL_STATIC_DRAW));
+
+    GLCall(glEnableVertexAttribArray(0));
+	GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, nullptr));
+
+    unsigned int ibo; //Index Buffer Object
+    GLCall(glGenBuffers(1, &ibo));
+    GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
+    GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW));
+	
+    const auto source = ParseShader("res/shaders/Basic.shader");
+    const unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
+    GLCall(glUseProgram(shader));
+
+    GLCall(const int location = glGetUniformLocation(shader, "u_Color"));
+    ASSERT(location != -1);
+    GLCall(glUniform4f(location, 0.8f, 0.3f, 0.8f, 1.0f));
+
+    GLCall(glBindVertexArray(0));
+    GLCall(glUseProgram(0));
+    GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
+    GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
+	
+    float r = 0.0f;
+    float increment = 0.05f;
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        GLCall(glClear(GL_COLOR_BUFFER_BIT));
+
+        GLCall(glUseProgram(shader));
+		GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
+    
+		GLCall(glBindVertexArray(vao));
+        GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
+    	
+        GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+
+        if (r > 1.0f)
+            increment = -0.05f;
+        else if (r < 0.0f)
+            increment = 0.05f;
+
+        r += increment;
+    	
+        /* Swap front and back buffers */
+        GLCall(glfwSwapBuffers(window));
+
+        /* Poll for and process events */
+        GLCall(glfwPollEvents());
+    }
+
+    GLCall(glDeleteProgram(shader));
+    glfwTerminate();
+	
+    return 0;
+}
